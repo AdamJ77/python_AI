155fbdd6cc14c4afe213e0047eb82f4da68e3f20
Adam <a.gepard007@gmail.com>
2022-10-14 00:48:44 +0200

    Checking the branches

--- wsi1.py
+++ wsi1.py
@@ -4,9 +4,9 @@ from matplotlib import pyplot as plt
 
 import time
 
-weights = np.array([8, 3, 5, 2])
+weights = np.array([8, 3, 5, 2, 8, 3, 5, 2, 16, 8, 9, 6, 8, 3, 5, 2])
 capacity = 9
-profits = np.array([16, 8, 9, 6])
+profits = np.array([16, 8, 9, 6, 8, 3, 5, 2, 8, 3, 5, 2, 8, 3, 5, 2])
 
 # plot_weights =
 
@@ -21,7 +21,7 @@ class KnapSack:
     might as well sort zip list with lambda on occurrences if 0 then stops already iterating, if 1 it still goes
     is sorting actually faster than going through each 0?
     """
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     variations = it.product(range(2), repeat=len(profits))
     max_sum_profits = 0
     max_sum_weight = 0
@@ -41,7 +41,7 @@ class KnapSack:
         max_sum_profits = curr_sum_profit
         max_sum_weight = curr_sum_weight
         final_indexes = indexes.copy()
-    running_time = time.process_time() - start_time
+    running_time = time.perf_counter() - start_time
     return {"Max profit": max_sum_profits, "Max weight": max_sum_weight, "Indexes": final_indexes}, f"{running_time:.5f}"
 
   def solve_knapsack_pw_ratio(self):
@@ -61,4 +61,3 @@ if __name__ == "__main__":
   information_value, time = knapsack.solve_knapsack_brute_force()
   print(information_value)
   print(time)
-  print("test")

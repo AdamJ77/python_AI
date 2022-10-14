18a598e266d0a8a25d5fb4955c5ef5c562796032
adam <a.gepard007@gmail.com>
2022-10-14 13:17:54 +0200

    Added plot generator

--- wsi1.py
+++ wsi1.py
@@ -2,13 +2,13 @@ import numpy as np
 import itertools as it
 from matplotlib import pyplot as plt
 
+import random
 import time
 
 weights = np.array([8, 3, 5, 2])
 capacity = 9
 profits = np.array([16, 8, 9, 6])
 
-# plot_weights =
 
 class KnapSack:
   def __init__(self, profits, weights, capacity):
@@ -21,7 +21,7 @@ class KnapSack:
     might as well sort zip list with lambda on occurrences if 0 then stops already iterating, if 1 it still goes
     is sorting actually faster than going through each 0?
     """
-    start_time = time.process_time()
+    start_time = time.monotonic()
     variations = it.product(range(2), repeat=len(profits))
     max_sum_profits = 0
     max_sum_weight = 0
@@ -41,23 +41,32 @@ class KnapSack:
         max_sum_profits = curr_sum_profit
         max_sum_weight = curr_sum_weight
         final_indexes = indexes.copy()
-    running_time = time.process_time() - start_time
-    return {"Max profit": max_sum_profits, "Max weight": max_sum_weight, "Indexes": final_indexes}, f"{running_time:.5f}"
+    running_time = time.monotonic() - start_time
+    return {"Max profit": max_sum_profits, "Max weight": max_sum_weight, "Indexes": final_indexes}, running_time
 
   def solve_knapsack_pw_ratio(self):
     pass
 
+def get_plot_brute_force(knapsack, n_elements):
+  times = []
+  n_profits = []
+  for _ in range(n_elements):
+    rand_profit = random.randint(1, 20)
+    rand_weight = random.randint(1, 10)
+    knapsack.profits = np.append(knapsack.profits, rand_profit)
+    knapsack.weights = np.append(knapsack.weights, rand_weight)
+    n_profits.append(len(knapsack.weights))
+    info, time_run = knapsack.solve_knapsack_brute_force()
+    times.append(time_run)
+  plt.plot(n_profits, times, linewidth=2.0)
+  plt.ylabel("Time")
+  plt.xlabel("Number of elements")
+  plt.show()
+  plt.savefig("Plot.png")
 
-  def get_plot_brute_force(self):
-    times = []
-    profits_plot = np.array([10])
-    weights_plot = np.array([1])
-    for _ in range(10):
-
-      pass
 
 if __name__ == "__main__":
   knapsack = KnapSack(profits, weights, capacity)
-  information_value, time = knapsack.solve_knapsack_brute_force()
+  information_value, time_run = knapsack.solve_knapsack_brute_force()
   print(information_value)
-  print(time)
+  get_plot_brute_force(knapsack, 10)
